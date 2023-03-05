1f0a71f3317db63b77e353ecea31bdb0ed91c313
exponenta.euler <exponenta.euler@gmail.com>
2023-03-05 15:31:50 +0500

    empty string check

--- TableDatabase/Form1.cs
+++ TableDatabase/Form1.cs
@@ -123,7 +123,8 @@ namespace TableDatabase
 
         private void dataGridView1_CellValidating(object sender, DataGridViewCellValidatingEventArgs e)
         {
-            if (double.TryParse(e.FormattedValue.ToString(), out _)) return;
+            if (double.TryParse(e.FormattedValue.ToString(), out _) ||
+                e.FormattedValue.ToString() == string.Empty) return;
             MessageBox.Show("Cell can contain only numerical value. Check your data.", "Incorrect value",
                 MessageBoxButtons.OK,
                 MessageBoxIcon.Error);
